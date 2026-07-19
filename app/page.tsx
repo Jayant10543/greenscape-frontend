400092533db629327eee50553d1cae63cd23738d
Jayant10543 <jayant.24bai10543@vitbhopal.ac.in>
2026-07-19 21:41:33 +0530

    Minor formatting in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
+
 import {
   Sprout, Microscope, Map, User, Search, Heart,
   Thermometer, Droplets, Wind, Flame, CloudRain, CloudLightning,
